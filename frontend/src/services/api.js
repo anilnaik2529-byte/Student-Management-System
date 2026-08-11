385d72498961811e5e1ccb5da37c823f53c0d567
anilnaik2529-byte <anilnaik2529@gmail.com>
2026-08-11 12:22:52 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,12 +8,12 @@ api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-    config.headers.Authorization = `Bearer ${TaskPriorityChangeEvent}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
-  
 });
+
 api.interceptors.response.use(
   (response) => response,
   (error) => {
@@ -28,4 +28,5 @@ api.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
 export default api;
